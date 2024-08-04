8b4ca8f43705c736d40682aaa42506c2a45e57cc
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2024-08-04 08:56:30 +0530

    java: data types

--- Hello.java
+++ Hello.java
@@ -1,7 +1,22 @@
-class hello{
+class Hello{
 
 public static void main(String a[]){
 
-System.out.print("Hello World");
+    //variabls
+    int num1 = 5;
+    int num2 = 4;
+    int result = num1 + num2;
+
+    //data types
+    int num = 9;
+    byte by = 127;
+    short sh =556;
+    long l = 584l;
+    float f=5.8f;
+    double d = 5.8;
+    char c = 'k';
+    boolean b = true;
+
+// System.out.print(result);
 }
 }
