93db61e3c08b7dd81a04eb14f4b3ebb75ba7f0b7
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2024-08-05 16:27:11 +0530

    java: while loop

--- Hello.java
+++ Hello.java
@@ -132,5 +132,21 @@ System.out.println("Enter valid number");
 }
 
 
+//Loops
+//while loop
+int i = 1;
+while(i<=4){
+    System.out.println("Hi"+i);
+
+    int j=1;
+    while(j<=3){
+        System.out.println("Hello"+j);
+        j++;
+    }
+    i++;
+}
+System.out.println("bye"+i);
+
+
 }
 }
