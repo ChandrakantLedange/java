3a974acaac9ed3554dcf2bcdc6955037831b53f4
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2024-08-10 18:02:05 +0530

    java: methods

--- Demo.java
+++ Demo.java
@@ -1,23 +1,46 @@
-// class and objects.
+// // class and objects.
 
-class Calculator{
-    int a; //variable
-    public int add(int n1, int n2){ //method
-        int r = n1 + n2;
-        return r;
+// class Calculator{
+//     int a; //variable
+//     public int add(int n1, int n2){ //method
+//         int r = n1 + n2;
+//         return r;
+//     }
+// }
+
+// public class Demo{
+// public static void main(String a[]){
+//     int num1 = 4;
+//     int num2 = 5;
+
+//     Calculator calc = new Calculator();// instance, 
+//     //calc is a variable.
+
+//     int result = calc.add(num1, num2);
+
+//     System.out.println(result);//9
+// }
+// }
+
+
+//Methods-
+class Computer{
+
+    public void playMusic(){
+     System.out.println("Music playing...");
+
     }
+    public String bugAPen(int cost){
+        return "Pen";
+    }
+    
 }
 
 public class Demo{
 public static void main(String a[]){
-    int num1 = 4;
-    int num2 = 5;
-
-    Calculator calc = new Calculator();// instance, 
-    //calc is a variable.
-
-    int result = calc.add(num1, num2);
-
-    System.out.println(result);//9
+    Computer obj  = new Computer();
+    obj.playMusic();//music playing
+    String str = obj.bugAPen(10);
+    System.out.println(str);//pen
 }
 }
