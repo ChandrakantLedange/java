cee2ed671ddc90d9e59b158f5f10025399c1b124
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2024-09-07 19:14:05 +0530

    java: multi-dimensional array

--- Demo.java
+++ Demo.java
@@ -84,22 +84,39 @@
 
 // Array - collection of variable values
 
+// public class Demo
+// {
+//     public static void main(String a[])
+//     {
+//         int nums[] = {3,7,2,4};
+//         System.out.println(nums[1]); //7
+
+//         // change value
+//         nums[1] = 6;
+//         System.out.println(nums[1]); //6
+
+//         //to print all values
+//         for(int i = 0; i < 4; i++){
+//         System.out.println(nums[i]); // 3,6,2,4
+
+//         }
+
+//     }
+// }
+
+
+//Multi-dimensional array.
+
 public class Demo
 {
     public static void main(String a[])
     {
-        int nums[] = {3,7,2,4};
-        System.out.println(nums[1]); //7
-
-        // change value
-        nums[1] = 6;
-        System.out.println(nums[1]); //6
-
-        //to print all values
-        for(int i = 0; i < 4; i++){
-        System.out.println(nums[i]); // 3,6,2,4
-
-        }
-
+            int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+            for (int i = 0; i < myNumbers.length; ++i) {
+                for (int j = 0; j < myNumbers[i].length; ++j) {
+                     System.out.print(myNumbers[i][j] +" ");
+                }
+                 System.out.println();
+            }
     }
 }
